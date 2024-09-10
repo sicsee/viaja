c86fa15cd492c3de2876c5ece6287baa5226033b
sicsee <nicolasdsgodinho@gmail.com>
2024-09-10 15:42:45 -0300

    Add files via upload

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -4,7 +4,3 @@ $(document).ready(function() {
         $('#mobile-btn').find('i').toggleClass('fa-x')
     })
 });
-
-document.getElementById("btn-1").addEventListener("click", function() {
-    window.location.href = "#destino";
-});
